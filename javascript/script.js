e42bb1c6fbc5817effd276c5ed63db875144d954
aluisiofs <aluisiofelipedasilva1@gmail.com>
2024-08-10 22:04:55 -0300

    Teste de variaveis JS

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,14 @@
 console.log('Ola Mundo!, Mensagem do JS')
 console.log('Mensagem 2')
 
+//alert ("Mensagem para o usuario")
 /*alert("Pagina em processo inicial de construção!")*/
 
-const minhaVariavel = "Pagina em Construção"
-alert(minhaVarieavel)
+//const nomeVariavelConstante
+//const variavelConstante = "Pagina em Construção"
+
+//let || var
+let varaivelDinamica = "Manutenção"
+variavelConstante =  "Manutenção"
+
+alert(varaivelDinamica)
