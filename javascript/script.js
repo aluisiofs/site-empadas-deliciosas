918261e492ce628ae81fad849d1ec95b7749f400
aluisiofs <aluisiofelipedasilva1@gmail.com>
2024-08-10 22:11:07 -0300

    Testes de Variaveis JS

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
-console.log('Ola Mundo!, Mensagem do JS')
-console.log('Mensagem 2')
+//console.log('Ola Mundo!, Mensagem do JS')
+//console.log('Mensagem 2')
 
 //alert ("Mensagem para o usuario")
 /*alert("Pagina em processo inicial de construção!")*/
