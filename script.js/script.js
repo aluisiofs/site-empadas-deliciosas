f7171a48166434272a9c095369549b34eeec72e5
aluisiofs <aluisiofelipedasilva1@gmail.com>
2024-08-17 21:16:11 -0300

    Adicionado melhorias em tags navbar  -  css -Bootstrap - Substitiçao assets -

---
+++ script.js/script.js
@@ -0,0 +1,15 @@
+//console.log('Ola Mundo!, Mensagem do JS')
+//console.log('Mensagem 2')
+
+//alert ("Mensagem para o usuario")
+/*alert("Pagina em processo inicial de construção!")*/
+
+//const nomeVariavelConstante
+//const variavelConstante = "Pagina em Construção"
+
+//let || var
+let varaivelDinamica = "Pagina Web em manutenção..!"
+variavelConstante =  "Pagina Web em manutenção..! "
+
+
+alert(varaivelDinamica)
